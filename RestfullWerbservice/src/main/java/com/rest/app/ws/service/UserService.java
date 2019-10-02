22f0d19dc33b70893da3c54abdb6625befa15d77
AbdoAshraf <eng.abdulrahman.ashraf@gmail.com>
2019-10-02 09:28:42 +0200

    Adding support to custom security.
    User now become able to sign up and, the application now supports
    encrypt the provided password and save the encrypted password to the
    database weoooo XD

--- RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserService.java
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserService.java
@@ -1,8 +1,10 @@
 package com.rest.app.ws.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.rest.app.ws.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
 	public UserDto creartUser(UserDto userDto);
 }
--- RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserServiceImpl.java
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserServiceImpl.java
@@ -2,6 +2,9 @@ package com.rest.app.ws.service;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.rest.app.ws.shared.Utils;
 
@@ -16,11 +19,13 @@ public class UserServiceImpl implements UserService {
 	UserRepository userRepo;
 	@Autowired
 	Utils utils ;
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Override
 	public UserDto creartUser(UserDto userDto) {
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(userDto, userEntity);
-		userEntity.setEncryptedPassword("test");
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
 		userEntity.setEmailVerificationStatus(false);
 		String publicUserId = utils.generateUserId(30);
 		userEntity.setUserId(publicUserId);
@@ -29,6 +34,11 @@ public class UserServiceImpl implements UserService {
 		BeanUtils.copyProperties(savedUser, result);
 		
 		return result ;
+	}
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
