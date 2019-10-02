a24cc41d7dc6986bde0e55b1065367ea7b273284
AbdoAshraf <eng.abdulrahman.ashraf@gmail.com>
2019-10-02 06:19:07 +0200

    FirstComment to the service layer.
    Adding support for service user in the service layer

---
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserService.java
@@ -0,0 +1,8 @@
+package com.rest.app.ws.service;
+
+import com.rest.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+
+	public UserDto creartUser(UserDto userDto);
+}
---
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.rest.app.ws.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rest.app.ws.UserRepository;
+import com.rest.app.ws.io.entity.UserEntity;
+import com.rest.app.ws.shared.dto.UserDto;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepo;
+	@Override
+	public UserDto creartUser(UserDto userDto) {
+		UserEntity userEntity = new UserEntity();
+		BeanUtils.copyProperties(userDto, userEntity);
+		userEntity.setEncryptedPassword("test");
+		userEntity.setEmailVerificationStatus(false);
+		userEntity.setUserId("tesId");
+		UserEntity savedUser = userRepo.save(userEntity);
+		UserDto result = new UserDto();
+		BeanUtils.copyProperties(savedUser, result);
+		
+		return result ;
+	}
+
+}
