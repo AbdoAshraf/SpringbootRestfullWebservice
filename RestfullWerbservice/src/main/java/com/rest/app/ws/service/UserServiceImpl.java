e82c53571c7b46a3fcae3ab3c563486737234115
AbdoAshraf <eng.abdulrahman.ashraf@gmail.com>
2019-10-02 07:41:46 +0200

    modify the POM base package

--- RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserServiceImpl.java
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.rest.app.ws.service;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.rest.app.ws.shared.Utils;
 
 import com.rest.app.ws.UserRepository;
 import com.rest.app.ws.io.entity.UserEntity;
@@ -13,13 +14,16 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepo;
+	@Autowired
+	Utils utils ;
 	@Override
 	public UserDto creartUser(UserDto userDto) {
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(userDto, userEntity);
 		userEntity.setEncryptedPassword("test");
 		userEntity.setEmailVerificationStatus(false);
-		userEntity.setUserId("tesId");
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);
 		UserEntity savedUser = userRepo.save(userEntity);
 		UserDto result = new UserDto();
 		BeanUtils.copyProperties(savedUser, result);
