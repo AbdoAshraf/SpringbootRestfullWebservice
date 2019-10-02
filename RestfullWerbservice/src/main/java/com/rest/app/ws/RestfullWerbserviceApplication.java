1b56cfed02f569a9cb561cc3a10143a6502c7dfc
AbdoAshraf <eng.abdulrahman.ashraf@gmail.com>
2019-10-02 06:03:45 +0200

    First commit main

---
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/RestfullWerbserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rest.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWerbserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWerbserviceApplication.class, args);
+	}
+
+}
