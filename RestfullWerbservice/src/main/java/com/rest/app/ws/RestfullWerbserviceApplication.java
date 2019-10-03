27f5629a4b2d3ac6257e74fcaf9d590f141e0567
AbdoAshraf <eng.abdulrahman.ashraf@gmail.com>
2019-10-03 15:30:01 +0200

    Reading SECRET_TOKEN from a properties file rather than hardcoded it

--- RestfullWerbservice/src/main/java/com/rest/app/ws/RestfullWerbserviceApplication.java
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/RestfullWerbserviceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.rest.app.ws.io.entity.security.AppProperties;
+
 @SpringBootApplication
 public class RestfullWerbserviceApplication {
 
@@ -21,6 +23,11 @@ public class RestfullWerbserviceApplication {
 	public SpringApplicationContext springApplicationContext()
 	{
 		return new SpringApplicationContext();
+	}
+	@Bean(name="AppProperties")
+	public AppProperties getAppProperties()
+	{
+		return new AppProperties();
 	}
 
 }
