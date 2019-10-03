92615eb824323cab693a91f200c266a2754420c5
AbdoAshraf <eng.abdulrahman.ashraf@gmail.com>
2019-10-03 07:46:36 +0200

    Adding support for user login

---
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/io/entity/security/AuthenticationFilter.java
@@ -0,0 +1,77 @@
+package com.rest.app.ws.io.entity.security;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rest.app.ws.service.UserService;
+import com.rest.app.ws.shared.dto.UserDto;
+import com.rest.app.ws.ui.model.request.UserLoginRequestModel;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**************************************************************
+ * AuthenticationFilter class, this class should extend the
+ * UsernamePasswordAuthenticationFilter in order to authenticate
+ * user by his email(user name and password taking the advantage 
+ * of the spring security framework
+ **************************************************************/
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	private final AuthenticationManager authenticationManager;
+
+	private String contentType;
+    
+	public AuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+    /*
+     * the method in which we should attemp authentication 
+     * the method reads the request and mapping it the UserLoginRequestMode 
+     * in order to searching the DB for it.
+     */
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+			throws AuthenticationException {
+		try {
+
+			contentType = req.getHeader("Accept");
+
+			UserLoginRequestModel creds = new ObjectMapper().readValue(req.getInputStream(),
+					UserLoginRequestModel.class);
+
+			return authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+    /*
+     * this method returns the token which bla bla bla 
+     */
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+
+		String userName = ((User) auth.getPrincipal()).getUsername();
+
+		String token = Jwts.builder().setSubject(userName)
+				.setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET).compact();
+		res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+	}
+}
