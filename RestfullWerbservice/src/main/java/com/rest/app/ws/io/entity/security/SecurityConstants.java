19c28e7b06aac30f9d4b960a06182059fea40943
AbdoAshraf <eng.abdulrahman.ashraf@gmail.com>
2019-10-03 11:28:34 +0200

    fix token expiary time bug

--- RestfullWerbservice/src/main/java/com/rest/app/ws/io/entity/security/SecurityConstants.java
+++ RestfullWerbservice/src/main/java/com/rest/app/ws/io/entity/security/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.rest.app.ws.io.entity.security;
 
 
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 864000000; // 10 days
+    public static final long EXPIRATION_TIME = 30; // 10 days
     public static final long PASSWORD_RESET_EXPIRATION_TIME = 3600000; // 1 hour
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
